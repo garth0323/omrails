7c99afdeae44d717034b8b84e43e41f8b90ff87e
Garth Gaughan <garth0323@gmail.com>
2013-08-24 16:21:10 -0700

    User Profile Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Omrails::Application.routes.draw do
+  get "users/show"
   resources :pins
 
   devise_for :users
+  get 'users/:id' => 'users#show', as: :user
   get 'about' => 'pages#about'
   
   root :to => 'pins#index'
